fe014f0b91906e0f043d337aa0f3fb9523fb91a6
vanjazeli <vanjazeli@gmail.com>
2023-06-11 16:21:07 +0200

    feat: add open modals to modal-cards

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import header from './chunks/header';
 import bannerMain from './chunks/bannerMain';
 import imageText from './chunks/imageText';
 import cardModule from './chunks/cardModule';
+import modalCards from './chunks/modalCards';
 
 documentLoaded.init();
 header.init();
 bannerMain.init();
 imageText.init();
 cardModule.init();
+modalCards.init();
